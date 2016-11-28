e34b4fe910a20d0848c26bda9c4324d88fa208bc
morsonio <siejka.eryk@gmail.com>
2016-11-28 08:24:51 +0100

    creating domain models

---
+++ ProjektWypozyczalnia/Wypozyczalnia.Web/Models/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Wypozyczalnia.Web.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        [ForeignKey("Booking")]
+        public int BookingId { get; set; }
+
+        public virtual Booking Booking { get; set; }
+    }
+}
