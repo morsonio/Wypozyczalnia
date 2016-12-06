f6a67425f8b08336705f794d80a370aa1887eb56
morsonio <siejka.eryk@gmail.com>
2016-12-06 02:56:09 +0100

    added Client angular.js project

---
+++ Wypozyczalnia.Client/app/cars/carListCtrl.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    "use strict";
+    angular
+        .module("carsManagement")
+        .controller("CarListCtrl",
+                     CarListCtrl);
+
+    function CarListCtrl() {
+        var vm = this;
+
+        vm.cars = [
+            {
+                "carId": 1,
+                "carBrand": "Opel",
+                "carModel": "Vectra",
+                "carAvailable": "true"
+            },
+            {
+                "carId": 2,
+                "carBrand": "Mazda",
+                "carModel": "6",
+                "carAvailable": "false"
+            }
+        ];
+    }
+}());
