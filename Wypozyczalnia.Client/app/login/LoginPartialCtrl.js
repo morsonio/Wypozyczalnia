44edbfe36c003d286432e39fe6c31845f2f97cdb
morsonio <siejka.eryk@gmail.com>
2016-12-12 01:50:51 +0100

    login controller logic

---
+++ Wypozyczalnia.Client/app/login/LoginPartialCtrl.js
@@ -0,0 +1,20 @@
+﻿(function() {
+    var app = angular.module('login-partial', []);
+
+    app.controller('loginPartialCtrl', function () {
+        this.loginForm = {};
+
+        this.loginto = function (product) {
+            product.reviews.push(this.review);
+            this.review = {};
+        };
+    });
+
+    app.directive("loginPartial",
+        function() {
+            return {
+                restrict: 'E',
+                templateUrl: "login-partialView.html"
+            };
+        });
+}());
