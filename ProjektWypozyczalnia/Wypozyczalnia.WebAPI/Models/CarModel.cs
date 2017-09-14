a2d365f78d8914fb62741139808328b6e39fd79e
mzemlo <marcin.zemlo@outlook.com>
2017-09-14 21:26:32 +0200

    Revert "sprawdzam tylko jak to się obsługiwało"
    This reverts commit f7725097aac3a07f1a4767bd6ae260d9497f9fcc.

--- ProjektWypozyczalnia/Wypozyczalnia.WebAPI/Models/CarModel.cs
+++ ProjektWypozyczalnia/Wypozyczalnia.WebAPI/Models/CarModel.cs
@@ -10,6 +10,5 @@ namespace Wypozyczalnia.WebAPI.Models
         public int Id { get; set; }
         public string Brand { get; set; }
         public string Model { get; set; }
-        public 
     }
 }
